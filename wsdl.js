b7be29b93e4a06a053b4d327db6af264c4f3416b
SergeyPC <49168978+SergeyPC@users.noreply.github.com>
2020-09-27 23:12:48 +0300

    Update wsdl.js

--- wsdl.js
+++ wsdl.js
@@ -19,7 +19,44 @@ module.exports.BelaviaWSDL = function (httpUrl) {
 }
 
 
-module.exports.toJson = require('./xml2json');
+module.exports.toJson = function (xml) {
+
+  // Create the return object
+  var obj = {};
+
+  if (xml.nodeType == 1) { // element
+    // do attributes
+    if (xml.attributes.length > 0) {
+    obj["@attributes"] = {};
+      for (var j = 0; j < xml.attributes.length; j++) {
+        var attribute = xml.attributes.item(j);
+        obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+      }
+    }
+  } else if (xml.nodeType == 3) { // text
+    obj = xml.nodeValue;
+  }
+
+  // do children
+  if (xml.hasChildNodes()) {
+    for(var i = 0; i < xml.childNodes.length; i++) {
+      var item = xml.childNodes.item(i);
+      var nodeName = item.nodeName;
+      if (typeof(obj[nodeName]) == "undefined") {
+        obj[nodeName] = xmlToJson(item);
+      } else {
+        if (typeof(obj[nodeName].length) == "undefined") {
+          var old = obj[nodeName];
+          obj[nodeName] = [];
+          obj[nodeName].push(old);
+        }
+        obj[nodeName].push(xmlToJson(item));
+      }
+    }
+  }
+  return obj;
+};
+
 module.exports.toXml = require('./json2xml');
 
 
