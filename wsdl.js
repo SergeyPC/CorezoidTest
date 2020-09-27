16a11434111d442d33c2801ac56972cc0c9e9b64
SergeyPC <49168978+SergeyPC@users.noreply.github.com>
2020-09-27 18:23:14 +0300

    Update wsdl.js

--- wsdl.js
+++ wsdl.js
@@ -1,6 +1,26 @@
 const BelaviaWSDL = 'GoodBoy';
+const http = require('http');
 
 module.exports.masha = function() {
     return BelaviaWSDL;
+}
+
+
+module.exports.misha = function() {
+    return new Promise(function(resolve, reject) {
+        var req = http.get('http://api-tt.belavia.by/TimeTable/Service.asmx?WSDL', function(res) {
+            let body = '';
+            res.on('data', function(chunk) {
+                body += chunk;
+            });
+            res.on('end', function() {
+                resolve(body);
+            });
+        });
+        req.on('error', function(err) {
+            reject(err);
+        });
+        req.end();
+    });
 }
 
