cabc84c71b204d2f778c33d8bb2092f522f38907
SergeyPC <49168978+SergeyPC@users.noreply.github.com>
2022-08-31 17:41:24 +0300

    Update usercode.js

--- usercode.js
+++ usercode.js
@@ -1,17 +1,16 @@
 const Vonage = require('@vonage/server-sdk');
 const VONAGE_API_KEY = '89e001ee';
 const VONAGE_API_SECRET = '9jXvJWWHeqQkK2qd';
-const VONAGE_APPLICATION_PRIVATE_KEY_PATH = '../private.key';
+const VONAGE_APPLICATION_PRIVATE_KEY_PATH = './private.key';
+
+const vonage = new Vonage({
+      apiKey: VONAGE_API_KEY,
+      apiSecret: VONAGE_API_SECRET,
+      applicationId: "0867d11e-b533-4d07-bcf2-682b734064b8",
+      privateKey: VONAGE_APPLICATION_PRIVATE_KEY_PATH
+});
 
 module.exports = (data) => {
-    
-    const vonage = new Vonage({
-        apiKey: VONAGE_API_KEY,
-        apiSecret: VONAGE_API_SECRET,
-        applicationId: "0867d11e-b533-4d07-bcf2-682b734064b7",
-        privateKey: VONAGE_APPLICATION_PRIVATE_KEY_PATH
-    });
-    
     vonage.calls.create({
         to: [{
             type: 'phone',
