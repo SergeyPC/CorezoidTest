8917e320888e9a89e8d1548f302a88731965911b
SergeyPC <moefedorovna@gmail.com>
2022-10-02 10:58:58 +0300

    fix5

--- usercode.js
+++ usercode.js
@@ -10,60 +10,50 @@ const vonage = new Vonage({
   privateKey: VONAGE_APPLICATION_PRIVATE_KEY_PATH
 });
 
-module.exports = (data) => {
-  return new Promise((resolve, reject) => {
-    vonage.calls.create({
-      to: [{
-        type: 'phone',
-        number: "380935441781"
-      }],
-      from: {
-        type: 'phone',
-        number: "380682886493"
-      },
-      ncco: [{
-        "action": "talk",
-        "text": "Do you confirm registration in the weather chat bot press 1 and lattice?"
-      },
-      {
-        action: 'input',
-        type: ['dtmf'],
-        eventUrl: ['https://www.corezoid.com/api/1/json/public/1126597/8abf74d6564ee21485b5d101a6354feca91c07a5'],
-        dtmf: {
-          'timeOut': '10',
-          'submitOnHash': true
-        }
+
+let calls = new Promise((resolve, reject) => {
+  vonage.calls.create({
+    to: [{
+      type: 'phone',
+      number: "380935441781"
+    }],
+    from: {
+      type: 'phone',
+      number: "380682886493"
+    },
+    ncco: [{
+      "action": "talk",
+      "text": "Do you confirm registration in the weather chat bot press 1 and lattice?"
+    },
+    {
+      action: 'input',
+      type: ['dtmf'],
+      eventUrl: ['https://www.corezoid.com/api/1/json/public/1126597/8abf74d6564ee21485b5d101a6354feca91c07a5'],
+      dtmf: {
+        'timeOut': '10',
+        'submitOnHash': true
       }
-      ]
-    }, (error, response) => {
-      if (error) {
-        console.log(error);
-        data.callAnswer = error;
-        reject(data);
-      }
-      if (response) {
-        console.log(response);
-        data.callAnswer = response;
-        resolve(data);
-      }
-    })
+    }
+    ]
+  }, (error, response) => {
+    if (error) {
+      console.error(error);
+      reject(error);
+    }
+    if (response) {
+      console.log(response);
+      resolve(response);
+    }
   })
-/*
-  return calls.then(resp => {
-    return new Promise((resolve, reject) => {
-      vonage.calls.get(resp.uuid, (err, res) => {
-        if (err) {
-          console.log(err);
-          data.callAnswer = err;
-          reject(data);
-        }
-        else {
-          console.log(res);
-          data.callAnswer = res;
-          resolve(data);
-        }
-      })
-    })
+})
+
+return calls.then((resp) => {
+  return vonage.calls.get(resp.uuid, (err, res) => {
+    if (err) {
+      return err; 
+    }
+    else {
+      return res;
+    }
   })
-  */
-}
+})
